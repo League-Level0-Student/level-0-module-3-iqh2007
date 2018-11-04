29bcab7105dcde3aac69459d852e2ab895fa9907
11000783 <josephaguilar2022@gmail.com>
2018-11-04 13:00:20 -0800

    ShinyObjects

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -1,5 +1,4 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
+
 
 package loops;
 
@@ -8,15 +7,30 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
+	private static int i;
+
 	public static void main(String[] args) {
+		
 		// 2. Ask the user how many shiny objects they want
+		
+String shiny = JOptionPane.showInputDialog(null, "How any shiny objects do you want?");
+		
+	int shinyobject = Integer.parseInt(shiny);	
 
 		// 3. Play the sound that many times
+		
+	for(int i=0; i<shinyobject; i++) {
+		
+		
 
 		// 1. Call the method below
+		
+		playMisterZee();	
+		}
 
 	}
 
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -1,29 +1,74 @@
 
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
-
 package loops;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
+
 	public static void main(String[] args) {
+
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+
+		// 1. Set the X position of the robot so that it starts on the left.
+
+		rob.setSpeed(20);
+		rob.penDown();
+		rob.setX(20);
+
 		// You also need to show the robot to see the result of this line.
 
 		// 2. Make the robot draw a star shape. Hint: 144.
 
-		// 3. Set the size of the star to 30.
+		for (int i = 0; i < 5; i++) {
 
-		/** THE CHALLENGE: **/
-		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walkOfFame
-		 * 
-		 * Hint: The distance between stars is 50.
-		 */
+			rob.move(50);
+			rob.turn(144);
 
+		}
+		rob.penUp();
+		rob.turn(-40);
+		rob.move(-100);
+		rob.penDown();
+
+		for (int i = 0; i < 5; i++) {
+			rob.move(70);
+			rob.turn(144);
+
+		}
+		
+		rob.penUp();
+		rob.turn(-10);
+		rob.move(-150);
+		rob.penDown();
+		for (int i = 0; i < 5; i++) {
+			rob.move(100);
+			rob.turn(144);
+
+		}
+		
+		rob.penUp();
+		rob.turn(-10);
+		rob.move(-200);
+		rob.penDown();
+		for (int i = 0; i < 5; i++) {
+			rob.move(120);
+			rob.turn(144);
+
+		}
+		
+		rob.penUp();
+		rob.turn(-10);
+		rob.move(-250);
+		rob.penDown();
+		for (int i = 0; i < 5; i++) {
+			rob.move(140);
+			rob.turn(144);
+
+		}
+		
 	}
 
+	// 3. Set the size of the star to 30.
+
+	int starSize = 30;
 }
