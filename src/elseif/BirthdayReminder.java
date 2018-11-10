fbf38c5dcc4ecf132241abaa402b3ab9e77dd9cd
11000783 <josephaguilar2022@gmail.com>
2018-11-10 10:38:15 -0800

    DizzyRobot

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,35 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "September 5th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String Birthday = JOptionPane.showInputDialog("Which birthday do you want to know?");
+
 		// 3. Print out what the user typed
-		
-		// 4. if user asked for "mom"
-			//print mom's birthday
-		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		System.out.println(Birthday);
 
-	} 
+		// 4. if user asked for "mom"
+		if (Birthday.equals("mom")) {
+			// print mom's birthday
+			System.out.println(momsBirthday);
+		}
+		// 5. if user asked for "dad"
+		
+		else if(Birthday.equals("dad")) {	
+		// print dad's birthday
+		System.out.println(dadsBirthday);
+		}
+		// 6. if user asked for your name
+		else if(Birthday.equals("mine")) {
+		// print myBirthday
+		System.out.println(myBirthday);							}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		
+	}
 }
